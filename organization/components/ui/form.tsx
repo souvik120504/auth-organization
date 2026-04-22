1194b5df3c4bbb6f4ae143f7cd0df737fd70e2a6
SouvikGit123 <souvikmitrapmisc2022@gmail.com>
2026-04-22 11:55:01 +0530

    Welcome

--- organization/components/ui/form.tsx
+++ organization/components/ui/form.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { Controller, FormProvider, useFormContext , useFormState } from "react-hook-form"
+import { Controller, FormProvider, useFormContext } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
@@ -21,24 +21,18 @@ FormItem.displayName = "FormItem"
 
 const FormLabel = Label
 
-const FormControl = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ ...props }, ref) => {
+const FormControl = React.forwardRef<any, any>(({ ...props }, ref) => {
   return <div ref={ref} {...props} />
 })
 FormControl.displayName = "FormControl"
 
-const FormMessage = ({ name }: { name: string }) => {
-  const {
-    formState: { errors },
-  } = useFormContext()
+const FormMessage = () => {
+  const { formState } = useFormContext()
+  const error = formState.errors
 
-  const error = errors?.[name]?.message as string | undefined
-
-  if (!error) return null
-
-  return <p className="text-sm text-red-500">{error}</p>
+  return error ? (
+    <p className="text-sm text-red-500"></p>
+  ) : null
 }
 
 export {
